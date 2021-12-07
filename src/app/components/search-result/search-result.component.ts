567becbf1a0b9b28b560f53b4dc1ae903b1c6d83
Refat Monir <refat.mounir@gmail.com>
2021-12-08 00:40:28 +0300

    handling dynamic filter using combine latest for ( source list , filter input ) observables

--- src/app/components/search-result/search-result.component.ts
+++ src/app/components/search-result/search-result.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { ColumnMode, SortType } from '@swimlane/ngx-datatable';
-import { Observable, Subscription } from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ColumnMode } from '@swimlane/ngx-datatable';
+import { combineLatest, Observable, of } from 'rxjs';
 import { Hero } from 'src/app/models/hero.model';
 import { HeroesSearchService } from 'src/app/services/heroes-search.service';
 
@@ -13,20 +14,32 @@ export class SearchResultComponent implements OnInit {
 
   @ViewChild('searchResults') table: any;
   heroesList: Hero[] = [];
+  heroesFilterSourc: Hero[] = [];
   expanded: any = {};
   timeout: any;
-  filterInput!:string;
-
+  //filterInput!:string;
+  a$: Observable<any> | undefined
   ColumnMode = ColumnMode;
 
+  form!: FormGroup;
+
   constructor(private searchService: HeroesSearchService) {
-   
+    this.form = new FormGroup(
+      { "filterInput": new FormControl('') }
+    )
+
   }
- 
+
   ngOnInit(): void {
-   
+    
+    let sub = this.form.get("filterInput")?.valueChanges.subscribe(text => {
+      const combined = combineLatest([of(this.heroesFilterSourc), of(text)]);
+      combined.subscribe(value => {
+        this.heroesList = value[0].filter(c => c.Name?.toLowerCase().includes(text.trim().toLowerCase()) || c.Company?.toLowerCase().includes(value[1].trim().toLowerCase()))
+      });
+    })
   }
-  
+
 
   toggleExpandRow(row: Hero) {
     this.table.rowDetail.toggleExpandRow(row);
